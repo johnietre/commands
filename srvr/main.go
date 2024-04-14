be9a4b694ab309a8ec5c3d19dea6ab9beabc4a22
johnietre <johnietrebus@gmail.com>
2024-04-14 18:27:01 -0500

    srvr rework

--- srvr/main.go
+++ srvr/main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	addrPtr := flag.String("addr", "localhost:8000", "Network address to listen on")
-	flag.StringVar(addrPtr, "a", "localhost:8000", "Network address to listen on")
-	flag.Parse()
-	println("Starting server on", *addrPtr)
-	println("Error:", http.ListenAndServe(*addrPtr, http.FileServer(http.Dir("."))).Error())
+	log.SetFlags(0)
+
+	rootCmd := &cobra.Command{
+		Use:   "srvr [address to run (default: 127.0.0.1:8000)] [--dir,-d dir]",
+		Short: "Serve a directory on the given address",
+		Run: func(cmd *cobra.Command, args []string) {
+			dir := cmd.Flag("dir").Value.String()
+			addr := cmd.Flags().Arg(0)
+			if addr == "" {
+				addr = "127.0.0.1:8000"
+			}
+
+			fmt.Printf("Serving %s on %s\n", addr, dir)
+			err := http.ListenAndServe(addr, http.FileServer(http.Dir(dir)))
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+	flags := rootCmd.Flags()
+	flags.StringP("dir", "d", ".", "Directory to serve")
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
